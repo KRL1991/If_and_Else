a5b3c2ffb5f614eaa3fc7821ec1490aa78bff032
KRL1991 <kimx4706@edu.easj.dk>
2020-09-28 09:55:42 +0200

    Added Switch

---
+++ src/Switch/Switch_example.java
@@ -0,0 +1,33 @@
+package Switch;
+
+public class Switch_example {
+
+    public static void main(String[] args) {
+
+        int jellybeans = 4;
+        switch (jellybeans) {
+            case 1:
+                System.out.println("Red Jellybean");
+                break;
+            case 2:
+                System.out.println("Blue Jellybean");
+                break;
+            case 3:
+                System.out.println("Green Jellybean");
+                break;
+            case 4:
+                System.out.println("Purple Jellybean");
+                break;
+            case 5:
+                System.out.println("Pink Jellybean");
+                break;
+            case 6:
+                System.out.println("Yellow Jellybean");
+                break;
+            case 7:
+                System.out.println("Black Jellybean");
+                break;
+        }
+
+    }
+}
