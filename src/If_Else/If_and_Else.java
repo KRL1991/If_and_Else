37cbcbc86c4618eadde88d7fd4ebffab25876e31
KRL1991 <kimx4706@edu.easj.dk>
2020-09-28 09:27:02 +0200

    Initial commit

---
+++ src/If_Else/If_and_Else.java
@@ -0,0 +1,19 @@
+package If_Else;
+
+public class If_and_Else {
+    // Eksempel på If - Else statement
+    public static void main(String[] args) {
+
+        int x = 75;
+        int y = 95;
+        if (x > y) {
+            System.out.println("X is greater than Y");
+            // Hvis X er større end Y printes ovenstående sætning
+
+            // Hvis Y er større printes der nedenstående sætning
+        } else {
+            System.out.println("X is not greater than Y.");
+        }
+    }
+
+}
